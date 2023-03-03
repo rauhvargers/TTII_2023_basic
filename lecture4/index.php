8045b37669bdbf35bae73203197402dfc13f9760
Kriss Rauhvargers <kriss.rauhvargers@lu.lv>
2023-03-03 10:40:32 +0000

    session data demo

--- lecture4/index.php
+++ lecture4/index.php
@@ -8,4 +8,13 @@ if ($example ==  "yes") {
     $output = "bad output";
 }
 
+//setcookie("testcookie", "testvalue", time()+1000, "/", "", false, true);
+//$_COOKIE["testcookie"] = "testvalue";
+
+session_start();
+
+$_SESSION["test"]  = "asdsdfds";
+$_SESSION["test2"]  = " qweqwe";
+$_SESSION["test3"] = [1,2,3,"bingo"];
+
 include("view.php");
