aecad877ffca1cf3abbeb873f9729de0fbeb6113
Kriss Rauhvargers <kriss.rauhvargers@lu.lv>
2023-03-03 11:18:30 +0000

    Validating user input

--- lecture4/index.php
+++ lecture4/index.php
@@ -8,6 +8,8 @@ if ($example ==  "yes") {
     $output = "bad output";
 }
 
+
+
 //setcookie("testcookie", "testvalue", time()+1000, "/", "", false, true);
 //$_COOKIE["testcookie"] = "testvalue";
 
@@ -15,6 +17,20 @@ session_start();
 
 $_SESSION["test"]  = "asdsdfds";
 $_SESSION["test2"]  = " qweqwe";
-$_SESSION["test3"] = [1,2,3,"bingo"];
+$_SESSION["test3"] = [1, 2, 3, "bingo"];
+
+$defaultyear = "";
+$defaultmonth = "";
+$error = "";
+
+if (isset($_GET["year"])) {
+    if (is_numeric( $_GET["year"] )) {
+        $defaultyear = $_GET["year"];
+    } else {
+        $error="The value you entered is not a number!";
+    }
+}
+
+
 
 include("view.php");
--- lecture4/view.php
+++ lecture4/view.php
@@ -1,14 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>View file demo</title>
 </head>
+
 <body>
-    <?php 
-        echo $output;
+    <?php
+    echo $output;
     ?>
+    <form action="/lecture4" method="get">
+        <?php
+        if ($error) {
+        ?>
+            <div class='error'>
+                <?php echo htmlspecialchars($error); ?>
+            </div>
+        <?php
+        }
+        ?>
+        Year: <input type="text" name="year" value="<?php echo htmlspecialchars($defaultyear); ?>" /> <br />
+        Month: <input type="text" name="month" value="<?php echo $defaultmonth ?>" />
+
+        <button type="submit">Submit</button>
+
+    </form>
+
 </body>
+
 </html>
